eea929724a6b0ff54b9a27369b771b504b0a60b6
Seyit Enes <seyit7enes@gmail.com>
2023-01-10 21:17:09 +0100

    not done

--- src/script.js
+++ src/script.js
@@ -1,44 +1,36 @@
-let myList = [];
-
 let income = 0;
 let expenses = 0;
+let balance = 0;
+const purhcaseName = document.getElementById("purchase-name");
+const purchasePrice = document.getElementById("purchase-price");
+const expensesOutput = document.getElementById("expenses");
 
+// eslint-disable-next-line func-names
 const calcBalance = function () {
-  const balance = income - expenses;
+  balance = income - expenses;
   document.getElementById("balance").innerText = balance;
 };
-
-//  function to toggle modal
+// eslint-disable-next-line func-names
 const toggleModal = function () {
   document.getElementById("overlay").classList.toggle("hidden");
   document.getElementById("modal").classList.toggle("hidden");
 };
-//  select the plus icon in the footer and add click event for modal
-document
-  .getElementById("add-icon")
-  .addEventListener("click", () => toggleModal());
-
-// select the x icon in the modal to close
-document
-  .getElementById("close-icon")
-  .addEventListener("click", () => toggleModal());
-
-// select the income box and add click event to type your income
-document.getElementById("income-section").addEventListener("click", () => {
-  const incomeDisplay = document.getElementById("income");
-  //  ask for income again if its not a number
-  do {
-    // eslint-disable-next-line no-alert
-    income = parseInt(prompt("type you income"), 10);
-    // eslint-disable-next-line no-restricted-globals
-  } while (isNaN(income));
-  incomeDisplay.innerText = `$ ${income}`;
-  calcBalance();
-});
-
-// blueprint for new purchase
+// eslint-disable-next-line func-names
+const editItems = function () {
+  document.querySelectorAll("#createdItem").forEach((item) => {
+    item.addEventListener("click", () => {
+      const itemPrice = item.querySelector("p");
+      console.log(itemPrice.innerText);
+      expenses -= itemPrice.innerText;
+      expensesOutput.innerText = expenses;
+      item.remove();
+    });
+    calcBalance();
+  });
+};
+// eslint-disable-next-line func-names
 const newPurchase = function (name, price) {
-  const nameOutput = document.createElement("p");
+  const nameOutput = document.createElement("h4");
   const priceOutput = document.createElement("p");
   const purchaseList = document.getElementById("purchase-list");
   const newItemWrapper = document.createElement("li");
@@ -54,38 +46,41 @@ const newPurchase = function (name, price) {
   purchaseList.appendChild(newItemWrapper);
   newItemWrapper.appendChild(nameOutput);
   newItemWrapper.appendChild(priceOutput);
-
   nameOutput.innerText = name;
-  priceOutput.innerText = `$ ${price} `;
+  priceOutput.innerText = `${price} `;
 };
 
-const editItems = function () {
-  document.querySelectorAll("#createdItem").forEach((item) => {
-    item.addEventListener("click", () => {
-      item.remove();
-    });
-  });
-};
+document
+  .getElementById("add-icon")
+  .addEventListener("click", () => toggleModal());
+
+document
+  .getElementById("close-icon")
+  .addEventListener("click", () => toggleModal());
+
+document.getElementById("income-section").addEventListener("click", () => {
+  const incomeDisplay = document.getElementById("income");
+  do {
+    // eslint-disable-next-line no-alert
+    income = parseInt(prompt("type you income"), 10);
+    // eslint-disable-next-line no-restricted-globals
+  } while (isNaN(income));
+  incomeDisplay.innerText = `${income}`;
+  calcBalance();
+});
 
 document.getElementById("submit").addEventListener("click", (event) => {
-  // prevent reloading
   event.preventDefault();
-  // select user input
-  const purhcaseName = document.getElementById("purchase-name");
-  const purchasePrice = document.getElementById("purchase-price");
-  expenses += parseInt(purchasePrice.value, 10);
-  document.getElementById("expenses").innerText = `$ ${expenses}`;
-  // add a new purchase
-  const addPurchase = newPurchase(
-    purhcaseName.value,
-    // eslint-disable-next-line comma-dangle
-    purchasePrice.value
-  );
+  // eslint-disable-next-line no-alert
+  if (!purhcaseName.value) return alert("type the price");
+  // eslint-disable-next-line no-alert
+  if (!purchasePrice.value) return alert("type the price");
 
-  myList.push(addPurchase);
-  calcBalance();
-  // close the modal
-  toggleModal();
-  myList = [];
+  expenses += parseInt(purchasePrice.value, 10);
+  expensesOutput.innerText = expenses;
+  // add a new purchase
+  newPurchase(purhcaseName.value, purchasePrice.value);
   editItems();
+  calcBalance();
+  toggleModal();
 });
